b49c8dc6906ee99c0af92ec06aa90f6290c7da4b
sagrika thakur <sagrikathakur68@gmail.com>
2026-06-09 16:22:07 +0530

    made a server

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log("server is running on port")
+})
