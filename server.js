ff1e99211adbd82ea8cc6557a0702cf92c926cdc
sagrika thakur <sagrikathakur68@gmail.com>
2026-06-10 13:14:02 +0530

    create operation using post http method

--- server.js
+++ server.js
@@ -4,14 +4,24 @@ import { pool } from './db.js'
 
 dotenv.config();
 
+const app = express();
 const port = process.env.PORT;
 
-const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
+
+app.get("/users", (req, res) => {
   res.send('hello world')
+})
+app.post('/product', (req, res) => {
+  const products = req.body;
+  console.log(products);
+  res.status(201).json({
+    message: "product created",
+    data: products
+  })
 })
 
 app.listen(port, () => {
-  console.log("server is running on port")
+  console.log(`server is running on port ${port}`)
 })
